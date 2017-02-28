f16d8ef5d2b33482497ae08a3ed0be6d1933b201
naterosenstock@gmail.com <nate@nate.nate>
2017-02-28 01:07:04 -0800

    Major cleanup, now beats Latitude most of the time

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var grid = [];
 var Board = require('../src/gridBuilder.js');
 var PF = require('pathfinding');
-var board;
-var data;
 
-/*
-TODO Next:
-- avoid head on collissions with larger snakes
-- when there's no path to food, it has no idea where to go
-    - maybe outline a path around the open space the snake is currently in
-*/
+// a few globals for now..
+var grid = [];
+var board;
+var reqBody = [];
 
 var numSnakes = 0;
 // Handle POST request to '/start'
@@ -30,40 +25,59 @@ router.post('/start', function(req, res) {
 // Handle POST request to '/move'
 router.post('/move', function(req, res) {
 
-    data = req.body;
+    reqBody = req.body;
     try {
-        var me = findMySnake(data);
 
-        var headX = me.coords[0][0];
-        var headY = me.coords[0][1];
+        var me = findMySnake();
 
-        var tailX = me.coords[me.coords.length - 1][0];
-        var tailY = me.coords[me.coords.length - 1][1];
+        var head = [me.coords[0][0], me.coords[0][1]];
+        var tail = [me.coords[me.coords.length - 1][0], me.coords[me.coords.length - 1][1]];
 
-        board = new Board(data.height);
-        board.addSnakes(data.snakes);
+        board = new Board(reqBody.height);
+        board.addSnakes(reqBody.snakes);
 
         // there might not be food sometime..
-        board.addFood(data.food);
-        var myPathsToFood = getPathsToFood(headX, headY, data, tailX, tailY);
+        board.addFood(reqBody.food);
 
-        console.log(board.grid);
-        console.log('My Head @ ' + headX + ', ' + headY);
-        console.log(myPathsToFood);
+        // of course add the auras..
+        addAuraToOtherSnakeHeads();
+
+        var myPathsToFood = getPathsToFood(head, tail);
+
+        board.print();
+
+        //console.log('Paths to food:');
+        //console.log(myPathsToFood);
+
+        console.log('My Head @ ' + head);
+        console.log('My Tail @ ' + tail);
+
+        var gotAMove = false;
         if (myPathsToFood.length > 0) {
-            // if it can't find paths to food, maybe it will go to arkadi's algortim
-            ///filterOutPathsWithoutEscapeRoutes(myPathsToFood);
+
             var myFoodTargetIndex = getShortestPathIndex(myPathsToFood);
             var myFoodPath = myPathsToFood[myFoodTargetIndex];
 
-            // is there food?
-            console.log('Heading to food @ ' + data.food[myFoodTargetIndex][0] + ', ' + data.food[myFoodTargetIndex][1]);
+            if (myFoodPath) {
 
-            myFoodPath.shift();
-            var nextCoordToFood = myFoodPath.shift();
-            var theMove = directionBetweenCoords(headX, headY, nextCoordToFood[0], nextCoordToFood[1]);
-        } else {
+                // is there food?
+                console.log('Heading to food @ ' + reqBody.food[myFoodTargetIndex]);
+
+                myFoodPath.shift();
+                var nextCoordToFood = myFoodPath.shift();
+                var theMove = directionBetweenCoords(head, nextCoordToFood);
+                gotAMove = true;
+            }
+        }
+        if (!gotAMove) {
             console.log('I HAVE NO PATH TO FOOD - I AM LOST');
+            // just move randomly as long as the place I move to has path to tail.
+            // otherwise who knows..
+
+            var target = getRandomFreeNeighbor(head);
+
+            console.log('HEADED TO TARGET ' + target);
+            var theMove = directionBetweenCoords(head, target);
         }
 
         console.log('I think I will move ' + theMove);
@@ -80,39 +94,147 @@ router.post('/move', function(req, res) {
         }
     }
 
-    var data = {
+    var responseData = {
 
         move: theMove, // one of: ['up','down','left','right']
         taunt: 'Outta my way, snake!', // optional, but encouraged!
     };
 
-    return res.json(data);
+    return res.json(responseData);
 });
 
+function isInBounds(coord) {
+    var x = coord[0];
+    var y = coord[1];
+    return (x >= 0 && x < reqBody.height) &&
+        (y >= 0 && y < reqBody.height);
+}
 
-function getPathsToFood(fromX, fromY, data, tailX, tailY) {
+// given a coord ([x,y]), returns an array of neighboring coordinates
+// that aren't out of bounds
+function getNeighborCoords(coord) {
+    var neighbors = [];
+
+    x = coord[0];
+    y = coord[1];
+
+    if (isInBounds([x + 1, y])) {
+        neighbors.push([x + 1, y]);
+    }
+
+    if (isInBounds([x, y + 1])) {
+        neighbors.push([x, y + 1]);
+    }
+
+    if (isInBounds([x - 1, y])) {
+        neighbors.push([x - 1, y]);
+    }
+
+    if (isInBounds([x, y - 1])) {
+
+        neighbors.push([x, y - 1]);
+    }
+
+    return neighbors;
+
+}
+// adds an aura to other snake heads - what else is there to say?
+// could try only adding the aura around heads of larger snakes than us,
+// but this way it helps with choke points too
+function addAuraToOtherSnakeHeads() {
+    for (snake of reqBody.snakes) {
+        if (snake.id !== reqBody.you) {
+            for (coord of getNeighborCoords(snake.coords[0])) {
+                gridBlock(coord);
+            }
+        }
+    }
+}
+
+function gridOpen(coord) {
+    board.grid[coord[1]][coord[0]] == 0;
+}
+
+// puts a '1' at a coordinate on the grid
+function gridBlock(coord) {
+    board.grid[coord[1]][coord[0]] = 1;
+}
+
+function isGridOpen(coord) {
+    return board.grid[coord[1]][coord[0]] == 0;
+}
+// TODO : if other snake head is a target, take the 1 off it on grid
+// givena coord, returns array of neighbors that are
+function getRandomFreeNeighbor(coord) {
+    var neighbors = getNeighborCoords(coord);
+    var result;
+    if (neighbors.length == 0) {
+        return -1; // no neighbors :(
+    }
+
+    for (coord of neighbors) {
+        if (isGridOpen(coord)) {
+            result = coord;
+            break;
+        }
+    }
+    return result;
+    //var randomIndex = Math.random() * neighbors.length;
+    //return neighbors[randomIndex];
+}
+// find a path from a to b
+// handles the grid cloning and other things
+// (which the docs for pathfinding algorithm state is necessary)
+function pathFind(a, b) {
+    var grid = clone2dArray(board.grid);
+
+    // NOTE: the coords a and b are zeroed on the grid before the search, if either are 1
+    // the search algorithm wont work
+    grid[a[1]][a[0]] = 0;
+    grid[b[1]][b[0]] = 0;
+
+    var pfgrid = new PF.Grid(grid);
+    var finder = new PF.AStarFinder();
+    var path = finder.findPath(a[0], a[1], b[0], b[1], pfgrid);
+
+    return path;
+}
+// for now Im just considering a path to food valid if theres a path to tail from it.
+// being picky about what food we're eating..
+function getPathsToFood(fromCoord, tail) {
     var foodPaths = [];
 
-    for (food of data.food) {
-        var x = food[0];
-        var y = food[1];
-        var pfgrid = new PF.Grid(board.grid);
-        var finder = new PF.AStarFinder();
-        var thisFoodPath = finder.findPath(fromX, fromY, x, y, pfgrid);
-
+    for (food of reqBody.food) {
         // make sure theres a path to tail first
-        if (hasPath([x, y], [tailX, tailY]))
+        var thisFoodPath = pathFind(fromCoord, food);
+        //console.log('path from food to tail? ');
+        if (hasPath(food, tail)) {
             foodPaths.push(thisFoodPath);
+        }
     }
     return foodPaths;
 }
+// probly a faster way to do this using Array.splice..
+function clone2dArray(grid) {
+    var newgrid = [];
 
-function hasPath(fromCoord, toCoord) {
-    var pfgrid = new PF.Grid(board.grid);
-    var finder = new PF.AStarFinder();
-    var thisFoodPath = finder.findPath(fromCoord[0], fromCoord[1], toCoord[0], toCoord[1], pfgrid);
+    for (var x = 0; x < grid.length; x++) {
+        newgrid[x] = [];
+        for (var y = 0; y < grid[x].length; y++) {
+            newgrid[x][y] = grid[x][y];
+        }
+    }
+    return newgrid;
 }
 
+function hasPath(fromCoord, toCoord) {
+    var path = pathFind(fromCoord, toCoord);
+    return path.length > 0;
+}
+
+// example of fromCoord/toCoord is array [19,19]
+// so for this function, we are going to set the source & destination to 0 on the temp grid,
+// otherwise there for sure wont be a path
 function getShortestPathIndex(arrayOfPaths) {
     var shortestPath = 99999;
     var index = -1;
@@ -132,7 +254,12 @@ function getShortestPathIndex(arrayOfPaths) {
 
 // only works if a move apart
 // eg 1,1 , 1,2
-function directionBetweenCoords(x1, y1, x2, y2) {
+function directionBetweenCoords(coord1, coord2) {
+    var x1 = coord1[0];
+    var y1 = coord1[1];
+
+    var x2 = coord2[0];
+    var y2 = coord2[1];
     //console.log('from ' + x1 + ' ' + y1 + ' ' + x2 + ' ' + y2);
 
     if (x2 > x1) {
@@ -158,10 +285,10 @@ function getRandomColor() {
     return color;
 }
 
-function findMySnake(data) {
+function findMySnake() {
     var mySnake;
-    for (snake of data.snakes) {
-        if (snake.id == data.you) {
+    for (snake of reqBody.snakes) {
+        if (snake.id == reqBody.you) {
             mySnake = snake;
         }
     }
--- src/gridBuilder.js
+++ src/gridBuilder.js
@@ -10,24 +10,35 @@ function Board(size) {
 }
 
 Board.prototype.addSnakes = function(snakes) {
-    snakes.forEach(function(snake) {
-        snake.coords.forEach(function(snakeCoords, i) {
+    for (snake of snakes) {
+        for (snakeCoords of snake.coords) {
             var x, y;
             x = snakeCoords[0];
             y = snakeCoords[1];
             this.grid[y][x] = 1;
-        }.bind(this));
-    }.bind(this));
+        }
+    }
+};
+
+Board.prototype.print = function() {
+    console.log(this.grid);
+    /*
+    for (var y = 0; y < this.size; y++) {
+        var row = [];
+        for (var x = 0; x < this.size; x++) {
+            row.push(this.grid[x][y]);
+        }
+        console.log(row);
+    }*/
 };
 
 Board.prototype.addFood = function(foods) {
-    foods.forEach(function(food) {
+    for (food of foods) {
         var x, y;
         x = food[0];
         y = food[1];
         this.grid[y][x] = 0;
-    }.bind(this));
+    }
 };
-
 
 module.exports = Board;
