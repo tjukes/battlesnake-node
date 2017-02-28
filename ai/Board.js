6c5248b05bc486fa75fadd9e687250dbb5d4e3fc
arkadi <arkadi81@gmail.com>
2017-02-28 11:20:55 -0800

    fixing board issues

--- ai/Board.js
+++ ai/Board.js
@@ -1,65 +1,72 @@
+/*jslint node: true */
+/*jshint esversion: 6 */
 'use strict';
+
+
 
 /** Class represents the board */
 class Board {
-  /**
- * Create a board object.
- * The board will keep track of the request information
- * and keep the game states and other useful useful information
- * infered from the current state.
- * @param {<Object>} body - the body of the request
- *
- */
-  	constructor(body) {
-    	this.height = body.height;
-    	this.width = body.width;
-    	this.food = body.food;
-    	this.game_id = body.game_id;
-    	this.turn = body.turn;
-    	this.you = body.you; // our snake id
-    	this.snakes = body.snakes;
+    /**
+     * Create a board object.
+     * The board will keep track of the request information
+     * and keep the game states and other useful useful information
+     * infered from the current state.
+     * @param {<Object>} body - the body of the request
+     *
+     */
+    addSnakes(snakes) {
 
-    	this.grid = [];
-    	//zero fill
-    	for (var x = 0; x < this.width; x++) {
-        	this.grid[x] = [];
-        	for (var y = 0; y < this.height; y++) {
-            	this.grid[x][y] = 0;
-        	}
-    	}
+        snakes.forEach(function(snake) {
+            snake.coords.forEach(function(snakeCoords, i) {
+                var x, y;
+                x = snakeCoords[0];
+                y = snakeCoords[1];
+                this.grid[y][x] = 1;
+            }.bind(this));
+        }.bind(this));
+    }
+
+
+
+    constructor(body) {
+        this.height = body.height;
+        this.width = body.width;
+        this.food = body.food;
+        this.game_id = body.game_id;
+        this.turn = body.turn;
+        this.you = body.you; // our snake id
+        this.snakes = body.snakes;
+
+        this.grid = [];
+        //zero fill
+        for (var x = 0; x < this.width; x++) {
+            this.grid[x] = [];
+            for (var y = 0; y < this.height; y++) {
+                this.grid[x][y] = 0;
+            }
+        }
 
         console.log('hi');
 
-    	addSnakes(this.snakes);
+        this.addSnakes(this.snakes);
+
+
 
         // is this working?
-        console.log(this.grid);
-        console.log('bye');
-    	// addFood(this.food); // can be added later - 
-    	// just need snakes to build walkability grid for pathfinder
-  	}
+        // addFood(this.food); // can be added later - 
+        // just need snakes to build walkability grid for pathfinder
+    }
 
-  	addSnakes(snakes) {
+    // addFood(foods) {
+    //  // todo, sub for instead of foreach later
+    //     foods.forEach(function(food) {
+    //         var x, y;
+    //         x = food[0];
+    //         y = food[1];
+    //         this.grid[y][x] = 2;
+    //     }.bind(this));
+    // };
 
-	    snakes.forEach(function(snake) {
-	        snake.coords.forEach(function(snakeCoords, i) {
-	            var x, y;
-	            x = snakeCoords[0];
-	            y = snakeCoords[1];
-	            this.grid[y][x] = 1;
-	        }.bind(this));
-	    }.bind(this));
-	};
-
-	// addFood(foods) {
-	// 	// todo, sub for instead of foreach later
-	//     foods.forEach(function(food) {
-	//         var x, y;
-	//         x = food[0];
-	//         y = food[1];
-	//         this.grid[y][x] = 2;
-	//     }.bind(this));
-	// };
 
 
 }
--- routes/index.js
+++ routes/index.js
@@ -15,26 +15,35 @@ router.post('/start', function (req, res) {
     name: "Ark-snake",
     head_url: "../spearhead.png", // optional, but encouraged!
     taunt: "eat all the things!!11~~", // optional, but encouraged!
-  }
+  };
 
-  return res.json(data)
-})
+  return res.json(data);
+});
 
 // Handle POST request to '/move'
 router.post('/move', function (req, res) {
   // NOTE: Do something here to generate your move
   // LOGIC COMES HERE - ARKADIS VERSION
 
-  var board = new Board(req.body); // information from request
 
-  console.log('making a move');
-  console.log('food present at ' + board.food);
+  try {
+    var board = new Board(req.body); // information from request
+    console.log('trying');
+    console.log('making a move');
+    console.log('food present at ' + board.food);
+    console.log(board.grid);
+  } catch(e) {
+    console.log(e.message);
+    
+  }
+
+  
   
   var data = {
     // move: chooseRandomMove(['up','down','left','right']),
     move: 'up', // options are up down left right
     taunt: "eat all the things!!11~~",
-  }
+  };
   return res.json(data);
 });
 
