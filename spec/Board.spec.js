e3257183236a3eea3336c59b707d8465ce59393c
Tana Jukes <tana.jukes@gmail.com>
2017-03-03 15:10:55 -0800

    Added missing semicolons in Board spec

--- spec/Board.spec.js
+++ spec/Board.spec.js
@@ -6,15 +6,15 @@ describe("Board", () => {
   var testBody = {width: 5, height: 5, snakes: []};
   beforeEach(() => {
     testBoard = new Board(testBody);
-  })
+  });
 
   it("creates a graph", () => {
-    var graph = testBoard.createGraph([[0,1],[0,0]])
+    var graph = testBoard.createGraph([[0,1],[0,0]]);
     var graphArray = [
       ["0,0", 0],["0,1", 1], ["1,0", 0], ["1,1", 0],
       [["0,0", "0,1"], 0], [["0,1", "0,0"], 1], [["0,0", "1,0"], 1], [["1,0", "0,0"], 1],
       [["1,1", "1,0"], 1], [["1,0", "1,1"], 1], [["1,1", "0,1"], 0], [["0,1", "1,1"], 1]
-    ]
-    expect(JSON.parse(graph.toJSON()).sort()).toEqual(graphArray.sort())
-  })
-})
+    ];
+    expect(JSON.parse(graph.toJSON()).sort()).toEqual(graphArray.sort());
+  });
+});
