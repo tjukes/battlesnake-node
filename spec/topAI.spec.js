7f363126f479345cdae9ac1911ae0769dcf10dec
Tana Jukes <tana.jukes@gmail.com>
2017-03-03 15:09:24 -0800

    Improve topAI spec
    Changed width and height values in the mock request from '2' to '20' (a typo in the API sample docs). Improved readability by condensing the coordinate lists to a single line each.

--- spec/topAI.spec.js
+++ spec/topAI.spec.js
@@ -6,7 +6,7 @@ describe("topAI", () => {
   beforeEach(() => {
     mockRequest = {
       "you": "25229082-f0d7-4315-8c52-6b0ff23fb1fb",
-      "width": 2,
+      "width": 20,
       "turn": 0,
       "snakes": [
         {
@@ -14,70 +14,33 @@ describe("topAI", () => {
           "name": "my-snake",
           "id": "25229082-f0d7-4315-8c52-6b0ff23fb1fb",
           "health_points": 93,
-          "coords": [
-            [
-              0,
-              0
-            ],
-            [
-              0,
-              0
-            ],
-            [
-              0,
-              0
-            ]
-          ]
+          "coords": [ [ 0, 0 ], [ 0, 0 ], [ 0, 0 ] ]
         },
         {
           "taunt": "gotta go fast",
           "name": "other-snake",
           "id": "0fd33b05-37dd-419e-b44f-af9936a0a00c",
           "health_points": 50,
-          "coords": [
-            [
-              1,
-              0
-            ],
-            [
-              1,
-              0
-            ],
-            [
-              1,
-              0
-            ]
-          ]
+          "coords": [ [ 1, 0 ], [ 1, 0 ], [ 1, 0 ] ]
+        },
+        {
+          "taunt": "gotta go fast",
+          "name": "another-snake",
+          "id": "0fd33b05-37dd-419e-b44f-af9936a0a00c",
+          "health_points": 75,
+          "coords": [ [ 1, 4 ], [ 1, 4 ], [ 1, 4 ] ]
         }
       ],
-      "height": 2,
+      "height": 20,
       "game_id": "870d6d79-93bf-4941-8d9e-944bee131167",
-      "food": [
-        [
-          1,
-          1
-        ]
-      ],
+      "food": [ [ 6, 6 ] ],
       "dead_snakes": [
         {
           "taunt": "gotta go fast",
           "name": "other-snake",
           "id": "c4e48602-197e-40b2-80af-8f89ba005ee9",
           "health_points": 50,
-          "coords": [
-            [
-              5,
-              0
-            ],
-            [
-              5,
-              0
-            ],
-            [
-              5,
-              0
-            ]
-          ]
+          "coords": [ [ 5, 0 ], [ 5, 0 ], [ 5, 0 ] ]
         }
       ]
     }
