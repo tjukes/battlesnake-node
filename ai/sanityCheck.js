e549ae0bb01f188b9d6c13ac87362aa851d70eb8
Tana Jukes <tana.jukes@gmail.com>
2017-03-04 14:57:22 -0800

    Add console logging for sanity check outcomes

--- ai/sanityCheck.js
+++ ai/sanityCheck.js
@@ -7,8 +7,10 @@ function sanityCheck(move, head, grid) {
   // if the destination is openand in bounds, go for it
   // otherwise, make an emergency move to a random open neighbour
   if (move && pathfindUtils.isCoordOpen(destination, grid) && pathfindUtils.isInBounds(destination, grid)) {
+    console.log('Sane move.');
     return move;
   } else {
+    console.log('Sanity failed! New plan...');
     var newCoord = pathfindUtils.getRandomFreeNeighbor(head, grid);
     var newMove = pathfindUtils.directionBetweenCoords(head, newCoord);
     return newMove;
