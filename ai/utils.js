a7de2d8897475e717c584478c0e3c1a1e06178f2
Tana Jukes <tana.jukes@gmail.com>
2017-03-04 12:08:52 -0800

    Linted - syntax & one functionality change
    Added missing semicolons and var declarations for iteration variables.
    Fixed for loop condition in function getNeighboursIndex (typo in accessing keys on grid).

--- ai/utils.js
+++ ai/utils.js
@@ -8,38 +8,38 @@ Snake = require("./Snake.js")
  */
 function _equiDistant(head, distance, height, width) {
   // First go around might have negative indices
-  cells = []
-  right = [head[0] + distance, head[1]]
-  top = [head[0], head[1] - distance]
-  left = [head[0] - distance, head[1]]
-  bottom = [head[0], head[1] + distance]
-  current = _.clone(right)
+  cells = [];
+  right = [head[0] + distance, head[1]];
+  top = [head[0], head[1] - distance];
+  left = [head[0] - distance, head[1]];
+  bottom = [head[0], head[1] + distance];
+  current = _.clone(right);
 
   // 1st quadrant
   while(!(current[0] == top[0] && current[1] == top[1])) {
-    current[0] -= 1
-    current[1] -= 1
-    cells.push(_.clone(current))
+    current[0] -= 1;
+    current[1] -= 1;
+    cells.push(_.clone(current));
   }
   // 2nd quadrant
   while(!(current[0] == left[0] && current[1] == left[1])) {
-    current[0] -= 1
-    current[1] += 1
-    cells.push(_.clone(current))
+    current[0] -= 1;
+    current[1] += 1;
+    cells.push(_.clone(current));
   }
   // 3rd quadrant
   while(!(current[0] == bottom[0] && current[1] == bottom[1])) {
-    current[0] += 1
-    current[1] += 1
-    cells.push(_.clone(current))
+    current[0] += 1;
+    current[1] += 1;
+    cells.push(_.clone(current));
   }
   // 4th quadrant
   while(!(current[0] == right[0] && current[1] == right[1])) {
-    current[0] += 1
-    current[1] -= 1
-    cells.push(_.clone(current))
+    current[0] += 1;
+    current[1] -= 1;
+    cells.push(_.clone(current));
   }
-  return cells
+  return cells;
 }
 
 /**
@@ -48,7 +48,7 @@ function _equiDistant(head, distance, height, width) {
  */
 function equiDistantFromHead(head, distance, height, width) {
    cells = _equiDistant(head, distance, width, height);
-   for (cell of cells) {
+   for (var cell of cells) {
      cell[0] = Math.min(cell[0], height - 1);
      cell[1] = Math.min(cell[1], width - 1);
      cell[0] = Math.max(cell[0], 0);
@@ -115,21 +115,21 @@ function aStar(graph, startVertex, endVertex) {
  * that aren't other snakes
  */
 function getNeighboursIndex(i, j, grid) {
-  var keys = {}
+  var keys = {};
   if (i > 0) {
-    keys.left = [i-1,j]
+    keys.left = [i-1,j];
   }
   if (i < grid.width) {
-    keys.right = [i+1,j]
+    keys.right = [i+1,j];
   }
   if (j > 0) {
-    keys.up = [i,j-1]
+    keys.up = [i,j-1];
   }
   if (j < grid.height) {
-    keys.down = [i,j+1]
+    keys.down = [i,j+1];
   }
-  for(l in keys) {
-    if (grid[keys[l][0], keys[l][1]] == 1) {
+  for (var l in keys) {
+    if (grid[keys[l][0]][keys[l][1]] === 1) {
       delete keys[l]; //delete prevents us from iterating over as opposed to setting to undefined
     }
   }
@@ -137,23 +137,23 @@ function getNeighboursIndex(i, j, grid) {
 }
 
 /**
- * Keeps track of when another snake eats, and
- * if planning on going into last element of tail, abort!
- * @param {Board} board
- */
+* Keeps track of when another snake eats, and
+* if planning on going into last element of tail, abort!
+* @param {Board} board
+*/
 
- function checkTailGrowth(board) {
-   // If currently neighbouring a tail
+function checkTailGrowth(board) {
+ // If currently neighbouring a tail
 
-   //Check the snakes head isn't right beside food!
- }
+ //Check the snakes head isn't right beside food!
+}
 
- /**
-  * Calculates taxi-car distance between two points
-  */
- function taxiDistance(startVertex, endVertex) {
-   return Math.abs(startVertex[0] - endVertex[0]) + Math.abs(startVertex[1] - endVertex[1])
- }
+/**
+* Calculates taxi-car distance between two points
+*/
+function taxiDistance(startVertex, endVertex) {
+ return Math.abs(startVertex[0] - endVertex[0]) + Math.abs(startVertex[1] - endVertex[1])
+}
 
 module.exports = {
   equiDistantFromHead,
